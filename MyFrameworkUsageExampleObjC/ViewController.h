6d05b92f13f1cd2a387f4afaa363028a0574ea46
Davide Bertola <dade@dadeb.it>
2017-04-03 15:57:24 +0200

    Initial Commit

---
+++ MyFrameworkUsageExampleObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFrameworkUsageExampleObjC
+//
+//  Created by Davide Bertola on 03/04/2017.
+//  Copyright © 2017 Davide Bertola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyFrameworkUsageExampleObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyFrameworkUsageExampleObjC
+//
+//  Created by Davide Bertola on 03/04/2017.
+//  Copyright © 2017 Davide Bertola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
